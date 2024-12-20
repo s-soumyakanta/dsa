c50b0820de17c59a327c89ad90732b0c19d21423
S Soumyakanta <mr.ssoumyakanta@gmail.com>
2024-12-20 09:51:13 +0530

    add merge two array

--- array/array.js
+++ array/array.js
@@ -47,3 +47,52 @@ for (let i = 0; i < searchArray.length - 1; i++) {
 }
 
 console.log(searchArray.indexOf(itemToSearch)) // -1 for not present value
+
+//Merge Arrays -
+
+let mergeArrayOne = [2, 3, 4]
+let mergeArrayTwo = [77, 99, 66]
+let arrayThree = []
+
+for (let i = 0; i < mergeArrayOne.length; i++) {
+    arrayThree.push(mergeArrayOne[i])
+}
+for (let i = 0; i < mergeArrayTwo.length; i++) {
+    arrayThree[mergeArrayOne.length + i] = mergeArrayTwo[i]
+}
+// arrayThree.reverse()
+const reverseArryThree = arrayThree.reverse()
+console.log(reverseArryThree)
+console.log(arrayThree)
+
+// Merege shortcut 
+const a1 = ["a", "b", "c"]
+const a2 = ["l", "m", "n"]
+const a3 = [...a1, ...a2]
+console.log(a3)
+
+//Merge Two arry - while loop
+
+let arrOne = [3, 5, 7, 45, 67]
+let arrTwo = [4, 6, 8]
+let arrThree = []
+let d1 = 0
+let d2 = 0
+let d3 = 0
+
+while (d1 < arrOne.length && d2 < arrTwo.length) {
+    if (arrOne[d1] < arrTwo[d2]) {
+        arrThree[d3] = arrOne[d1]
+        d1++;
+    } else {
+        arrThree[d3] = arrTwo[d2]
+        d2++;
+    }
+    d3++;
+}
+while (d1 < arrOne.length) {
+    arrThree[d3] = arrOne[d1]
+    d1++;
+    d3++;
+}
+console.log(arrThree)
--- array/array.md
+++ array/array.md
@@ -4,4 +4,8 @@
 
 https://talentbattle.in/learn-data-structures-for-free/arrays-in-data-structures/traversal-in-arrays
 
-Traversal is one of the fundamental operations in data structures, involving visiting each element systematically. In the context of arrays, traversal means accessing each element from the beginning to the end of the array. This operation is crucial for various tasks such as searching, updating, and processing data.
+Traversal is one of the fundamental operations in data structures, involving visiting each element systematically. In the context of arrays, traversal means accessing each eleme nt from the beginning to the end of the array. This operation is crucial for various tasks such as searching, updating, and processing data.
+
+Space complexity - 10 byte space - low space taking code in system is called space complexity
+
+Time Complexity - It depends upon sytem and no of line
